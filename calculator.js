c3417cbc677875c67a0e951b1a1606441626f882
drew-witt <55118872+drew-witt@users.noreply.github.com>
2021-01-30 13:00:55 -0800

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,11 @@
+let num1 = 100;
+let num2 = 200;
+let num3 = 500;
+let num4 = 1000;
+console.log(9 * num1);
+console.log(num2 ** 3);
+console.log(num4 - num3);
+console.log(num3 - num4);
+console.log(num4 / num1);
+console.log(num1 + num2 + num3 + num4);
+console.log((num4 * num3) / num1);
